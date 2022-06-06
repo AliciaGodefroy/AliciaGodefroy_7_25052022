9f23f02ee59837d6c37e0e5db8fb38a05c4f45a8
Alicia Godefroy <aliciagodefroy24@gmail.com>
2022-06-06 18:27:10 +0200

    vue first pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,22 @@
+/* eslint-disable */
+<script>
+import NavLink from './components/NavLink.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavLink,
+  },
+};
+</script>
+
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <NavLink url="/" text="Accueil" />
+      <NavLink url="/about" text="À propos" />
+      <NavLink url="/contact" text="Contact" />
     </nav>
-    <router-view/>
   </div>
 </template>
 
