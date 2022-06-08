93ab08933305c986fbfa249dfcfdc9c52e5b31fd
Alicia Godefroy <aliciagodefroy24@gmail.com>
2022-06-08 11:17:39 +0200

    header accueil

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,44 +1,64 @@
 /* eslint-disable */
-<script>
-import NavLink from './components/NavLink.vue';
-
-export default {
-  name: 'App',
-  components: {
-    NavLink,
-  },
-};
-</script>
-
 <template>
   <div id="app">
+    <header class="header">
+    <img src="./assets/icon-left-font-monochrome-white.png" alt="Logo Groupomania"
+    class="header__logo">
+    </header>
     <nav>
-      <NavLink url="/" text="Accueil" />
-      <NavLink url="/about" text="À propos" />
-      <NavLink url="/contact" text="Contact" />
+      <router-link to="/">Inscription</router-link>
+      <router-link to="/login">Connexion</router-link>
+      <!-- <NavLink url="/" text="Inscription" class="signup"/>
+      <NavLink url="/login" text="Connexion"  class="login"/> -->
     </nav>
   </div>
 </template>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:wght@400;500&family=Shrikhand&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Lato, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
+body {
+  background-color: rgba(253, 45, 1, 0.6);
+  background-image:url('assets/image_de_fond.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+}
+
 nav {
   padding: 30px;
+  color: #FD2D01;
+  text-decoration: none;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #FFFF;
+  text-decoration: none;
+  padding: 10px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #FD2D01;
+  background-color: #FFFF;
+  border-radius: 5px;
 }
+
+.header{
+  display: flex;
+  justify-content: center;
+  padding-top: 130px;
+  &__logo{
+    width: 230px ;
+  }
+}
+
 </style>
