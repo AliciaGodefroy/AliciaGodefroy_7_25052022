f8971a83d30f731d3ff56476d3a238faae9a083e
Alicia Godefroy <aliciagodefroy24@gmail.com>
2022-06-15 17:56:50 +0200

    postcard

--- frontend/src/components/PostCard.vue
+++ frontend/src/components/PostCard.vue
@@ -3,13 +3,38 @@
     <article class="card" v-for="post in posts" :key="post.id">
       <header>
         <div class="card__title">{{ post.pseudo }}</div>
-        <time class="card-date">
+        <time class="card__date">
           {{ "Publié le " + post.createdAt.slice(0, 10).split("-").reverse().join("/")
           + " à " + post.createdAt.slice(11, 16) }}
         </time>
       </header>
-      <section class="card__content">
-      </section>
+      <div class="card__content">
+        <p v-show="post.message">
+            {{ post.message }}
+        </p>
+        <div v-if="post.imageUrl">
+          <img :src="post.imageUrl" alt="Image publiée" />
+        </div>
+      </div>
+      <footer>
+
+      </footer>
     </article>
   </div>
 </template>
+
+<script>
+// import Axios from 'axios';
+
+// import Like from '../assets/heart-solid.svg';
+
+export default ({
+  name: 'PostCard',
+  // components: { Like },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+});
+</script>
