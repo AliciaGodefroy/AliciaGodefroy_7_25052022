81b0619569d56db452863e048801b6e49026010f
Alicia Godefroy <aliciagodefroy24@gmail.com>
2022-06-20 23:39:03 +0200

    posts

--- frontend/src/components/PostCard.vue
+++ frontend/src/components/PostCard.vue
@@ -3,13 +3,20 @@
     <article class="card" v-for="post in posts" :key="post._id">
       <header class="card__header">
         <p class="card__pseudo">{{ pseudo }}</p>
-        <a class="link" :href="'/publication/edit/' + post._id">
-        <img src="../assets/pen-to-square-solid.svg" alt="Icon Modifier" class="card__icon"></a>
+        <div class="icons">
+          <a class="link" :href="'/publication/edit/' + post._id">
+            <img src="../assets/pen-to-square-solid.svg" alt="Icon Modifier" class="card__icon">
+          </a>
+          <img src="../assets/trash-can-solid.svg" alt="Icon Supprimer"
+          @click="deletePost(post)" class="card__icon" aria-hidden="true">
+        </div>
       </header>
       <p class="card__content" v-show="post.text">{{ post.text }}</p>
-      <img :scr="post.imageUrl" alt= 'image publiée' class= "card__image"/>
-      <footer>
-
+      <img :src="post.imageUrl" alt= 'image publiée' class= "card__image"/>
+      <footer class="card__footer">
+        <a class="link" :href="'/publication/edit/' + post._id">
+          <img src="../assets/heart-regular.svg" alt="Icon Modifier" class="card__icon heart">
+        </a>
       </footer>
     </article>
   </div>
@@ -17,14 +24,9 @@
 
 <script>
 import Axios from 'axios';
-// Import des icons
-// import Edit from '../assets/pen-to-square-solid.svg';
 
 export default {
   name: 'PostCard',
-  // components: {
-  //   'edit-post': Edit,
-  // },
   // eslint-disable-next-line object-shorthand
   data: function () {
     return {
@@ -34,28 +36,73 @@ export default {
   },
   mounted() {
     this.getPseudo();
-    const user = JSON.parse(localStorage.getItem('user'));
-    const AccessToken = user.token;
-    // eslint-disable-next-line prefer-template
-    const header = { headers: { Authorization: 'Bearer ' + AccessToken } };
-    // eslint-disable-next-line  prefer-template
-    Axios
-      // eslint-disable-next-line  prefer-template
-      .get('http://localhost:3000/api/post/', header)
-      .then(
-        // eslint-disable-next-line no-return-assign
-        (response) => (
-          // eslint-disable-next-line no-sequences
-          (this.posts = response.data),
-          (this.user = JSON.stringify(response.data.userId))
-        ),
-      )
-      .catch((error) => console.log(error));
+    this.getAllPosts();
+    this.userId = localStorage.getItem('userId');
+    this.isAdmin = localStorage.getItem('isAdmin');
   },
   methods: {
-    getPseudo() { // On récupère le pseudo depuis le localStorage
+    // Récupérer le pseudo de l'utilisateur depuis le localStorage
+    getPseudo() {
       const user = JSON.parse(localStorage.getItem('user'));
       this.pseudo = user.pseudo;
+    },
+    // Afficher tous les posts
+    getAllPosts() {
+      const user = JSON.parse(localStorage.getItem('user'));
+      const AccessToken = user.token;
+      // eslint-disable-next-line prefer-template
+      const header = { headers: { Authorization: 'Bearer ' + AccessToken } };
+      // eslint-disable-next-line  prefer-template
+      Axios
+        // eslint-disable-next-line  prefer-template
+        .get('http://localhost:3000/api/post/', header)
+        .then(
+          // eslint-disable-next-line no-return-assign
+          (response) => (
+            // eslint-disable-next-line no-sequences
+            (this.posts = response.data),
+            (this.user = JSON.stringify(response.data.userId))
+          ),
+        )
+        .catch((error) => console.log(error));
+    },
+    // Modifier une publication
+    editPublication(post) {
+      const user = JSON.parse(localStorage.getItem('user'));
+      const AccessToken = user.token;
+      Axios
+        // eslint-disable-next-line prefer-template
+        .get('http://localhost:3000/api/post/' + post.id, { headers: { Authorization: 'Bearer ' + AccessToken } })
+        .then((res) => {
+          console.log('This is the res from get message/id');
+          console.log(res);
+          // eslint-disable-next-line  prefer-arrow-callback
+          this.$router.push({ name: 'EditPage' }); // Redirection vers la page d'Accueil
+        })
+        // eslint-disable-next-line  prefer-arrow-callback
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    // Supprimer une publication
+    deletePost(post) {
+      const user = JSON.parse(localStorage.getItem('user'));
+      const AccessToken = user.token;
+      Axios
+        // eslint-disable-next-line prefer-template
+        .delete('http://localhost:3000/api/post/' + post.id, { headers: { Authorization: 'Bearer ' + AccessToken } })
+        .then((response) => {
+          console.log('this is response from deletePost');
+          console.log(response.data.message);
+        })
+        // On refait un get derrière pour MAJ
+        .then(() => {
+          this.getAllPosts();
+        })
+        .catch((err) => {
+          console.log('this is error from deletePost');
+          console.log(err);
+        });
     },
   },
 };
@@ -90,11 +137,24 @@ export default {
     display: flex;
   }
   &__image{
-    padding: 20px;
+    padding: 10px;
+    object-fit: cover;
+    object-position: center;
+    width: 95%;
+    border-radius: 30px;
   }
   &__icon{
     height: 20px;
     padding: 10px 10px 0 10px;
+  }
+  &__footer{
+    display: flex;
+  }
+  .icons{
+    display: flex;
+  }
+  .heart{
+    margin-bottom: 10px;
   }
 }
 </style>
